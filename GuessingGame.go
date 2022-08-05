290f06b532e1812c2897df4b58bcd43fc2cd297f
Ben Taylor <ben.taylor2573@gmail.com>
2022-08-06 00:02:12 +0100

    guessing game

---
+++ GuessingGame.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func question(question string, answer string) int {
+	var guess string
+	fmt.Print(question + "\n--> ")
+	fmt.Scan(&guess)
+	if guess == answer {
+		fmt.Print("Correct\n\n")
+		return 1
+	} else {
+		fmt.Printf("Incorrect (%v)\n\n", answer)
+		return 0
+	}
+}
+
+func main() {
+	fmt.Print("Guessing Game\n\n")
+
+	score := 0
+	score += question("What's 5 + 5?", "10")
+	score += question("What's 5 - 5?", "0")
+
+	fmt.Printf("%v/2", score)
+}
